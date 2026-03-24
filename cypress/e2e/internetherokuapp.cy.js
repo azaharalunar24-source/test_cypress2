deb3e538166c24853e391b0ff758c788623a88a7
Azahara Leon <azaharalunar24@gmail.com>
2026-03-24 17:17:41 +0100

    segundo ejercicio

--- cypress/e2e/internetherokuapp.cy.js
+++ cypress/e2e/internetherokuapp.cy.js
@@ -14,7 +14,23 @@ describe('template spec', () => {
     cy.get('[name="password"]').type("SuperSecretPassword!")
     cy.get('#login button.radius').click()
     cy.get('.subheader')
-    cy.get('#flash')    
-})   
-
-    
+    cy.get('#flash')  
+})
+ it('ir a la pagina', () => {
+    cy.visit('https://the-internet.herokuapp.com/')  
+}) 
+it('clicar en shifting content', () => {
+    cy.visit('https://the-internet.herokuapp.com/')
+    cy.get(':nth-child(39)>a').click()
+})     
+it('Clicar en Example 1', () => {
+    cy.visit('https://the-internet.herokuapp.com/')
+    cy.get(':nth-child(39)>a').click()
+    cy.get('[href="/shifting_content/menu"]').click()
+}) 
+it('Comprobar que la tabla tiene 5 elementos', () => {
+    cy.visit('https://the-internet.herokuapp.com/')
+    cy.get(':nth-child(39)>a').click()
+    cy.get('[href="/shifting_content/menu"]').click()
+    cy.get('.example ul li').should('have.length', 5)
+}) 
