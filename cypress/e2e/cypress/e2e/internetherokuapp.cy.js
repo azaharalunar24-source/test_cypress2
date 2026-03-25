7c73ee7edecc640944cffef0a27f6b86d252386d
Azahara Leon <azaharalunar24@gmail.com>
2026-03-25 14:24:45 +0100

    internetherokuapp

--- cypress/e2e/cypress/e2e/internetherokuapp.cy.js
+++ cypress/e2e/cypress/e2e/internetherokuapp.cy.js
@@ -1,13 +1,6 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://the-internet.herokuapp.com/')
-  })
-})
-  it('clicar en form', () => {
-    cy.visit('https://the-internet.herokuapp.com/')
-    cy.get(':nth-child(21)>a').click()
-   })
- it('login', () => {
+  
+it('login', () => {
     cy.visit('https://the-internet.herokuapp.com/')
     cy.get(':nth-child(21)>a').click()
     cy.get('[name="username"]').type("tomsmith")
@@ -16,21 +9,13 @@ describe('template spec', () => {
     cy.get('.subheader')
     cy.get('#flash')  
 })
- it('ir a la pagina', () => {
-    cy.visit('https://the-internet.herokuapp.com/')  
-}) 
-it('clicar en shifting content', () => {
-    cy.visit('https://the-internet.herokuapp.com/')
-    cy.get(':nth-child(39)>a').click()
-})     
-it('Clicar en Example 1', () => {
-    cy.visit('https://the-internet.herokuapp.com/')
-    cy.get(':nth-child(39)>a').click()
-    cy.get('[href="/shifting_content/menu"]').click()
-}) 
 it('Comprobar que la tabla tiene 5 elementos', () => {
+    cy.visit('https://the-internet.herokuapp.com/')
+    cy.get(':nth-child(39)>a').click()    
+    cy.get('[href="/shifting_content/menu"]').click()
     cy.visit('https://the-internet.herokuapp.com/')
     cy.get(':nth-child(39)>a').click()
     cy.get('[href="/shifting_content/menu"]').click()
     cy.get('.example ul li').should('have.length', 5)
+}) 
 }) 
